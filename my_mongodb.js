5210109b0db3e651b81ceb51f3a9f7207163be6e
Denis Komnenovic <33510295+denikom72@users.noreply.github.com>
2023-10-24 11:41:29 +0200

    Update my_mongodb.js

--- my_mongodb.js
+++ my_mongodb.js
@@ -1,3 +1,8 @@
+```
+SELECT Category, AVG(Price) AS AvgPrice
+FROM Products
+GROUP BY Category;
+```
 db.Products.aggregate([
    { $group: { _id: "$Category", AvgPrice: { $avg: "$Price" } } }
 ]);
@@ -55,6 +60,80 @@ SELECT Category, AVG(Price) AS AvgPrice, MAX(Price) AS MaxPrice
 FROM Products
 GROUP BY Category;
 ```
+db.Products.aggregate([
+   { $group: { _id: "$Category", AvgPrice: { $avg: "$Price" }, MaxPrice: { $max: "$Price" } } }
+]);
+
+//
+
+db.sales.aggregate([
+  {
+    $match: {
+      OrderDate: {
+        $gte: ISODate("2023-01-01"),
+        $lte: ISODate("2023-12-31")
+      }
+    }
+  },
+  {
+    $lookup: {
+      from: "products",
+      localField: "ProductID",
+      foreignField: "ProductID",
+      as: "product"
+    }
+  },
+  {
+    $unwind: "$product"
+  },
+  {
+    $lookup: {
+      from: "categories",
+      localField: "product.CategoryID",
+      foreignField: "CategoryID",
+      as: "category"
+    }
+  },
+  {
+    $unwind: "$category"
+  },
+  {
+    $group: {
+      _id: "$category.CategoryName",
+      TotalSales: {
+        $sum: { $multiply: ["$UnitPrice", "$Quantity"] }
+      },
+      CustomerIDs: { $addToSet: "$CustomerID" }
+    }
+  },
+  {
+    $lookup: {
+      from: "customers",
+      localField: "CustomerIDs",
+      foreignField: "CustomerID",
+      as: "customers"
+    }
+  },
+  {
+    $unwind: "$customers"
+  },
+  {
+    $match: {
+      "customers.TotalPurchases": { $gt: 10000 }
+    }
+  },
+  {
+    $project: {
+      CategoryName: "$_id",
+      TotalSales: 1,
+      CustomerName: "$customers.CustomerName",
+      _id: 0
+    }
+  }
+]);
+
+
+   
 
 
 
